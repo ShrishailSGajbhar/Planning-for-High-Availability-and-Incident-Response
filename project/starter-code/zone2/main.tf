dc066044d4c2754f6773d0930ec5289335cecd99
ShrishailSGajbhar <shrishailgajbhar5@gmail.com>
2023-01-12 23:14:06 +0530

    Correction for zone2 folder

--- project/starter-code/zone2/main.tf
+++ project/starter-code/zone2/main.tf
@@ -42,10 +42,4 @@ locals {
 #   }
 # }
 
-module "project_alb" {
-  source    = "./modules/alb"
-  ec2       = module.project_ec2.aws_instance
-  ec2_sg    = module.project_ec2.ec2_sg
-  subnet_id = data.terraform_remote_state.vpc.outputs.public_subnet_ids
-  vpc_id    = data.terraform_remote_state.vpc.outputs.vpc_id
-}
+
